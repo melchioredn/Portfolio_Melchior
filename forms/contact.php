4d2749e9c9d6149ef5d3902810e393ffd098a739
Melchior Edoun <129172910+melchioredn@users.noreply.github.com>
2026-03-05 17:34:53 +0300

    Change SMTP password in contact.php
    Updated SMTP credentials in contact form.

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'edounmelchior4@gmail.com',
-    'password' => 'sgjb ysgz ihqy hymq',
+    'password' => 'bpis gjns pnpe lveu',
     'port' => '587'
   );
   
@@ -39,3 +39,4 @@
 
   echo $contact->send();
 ?>
+
